b0469d7cbf0d28b5f313df94d62e126429e3f696
Christopher Ritter <ritter.c@husky.neu.edu>
2019-09-23 14:53:33 -0400

    Init Commit

---
+++ osc.js
@@ -0,0 +1,32 @@
+const osc = require('osc');
+
+module.exports = function(wss) {
+  let udp = new osc.UDPPort({});
+
+  udp.on('ready', () => {
+    console.log('Listening for OSC over UDP.');
+  });
+
+  udp.on('message', (msg, timeTag, info) => {
+    console.log('Got UDP:');
+    console.log(msg);
+    console.log(timeTag);
+    console.log(info);
+    console.log('\n');
+    udp.options.remoteAddress = info.address;
+    udp.options.remotePort = info.port;
+  });
+
+  udp.on('error', (e) =>{
+    console.log(e);
+  });
+
+  udp.open();
+
+
+  wss.on('connection', (socket) => {
+    let socketPort = new osc.WebSocketPort({ socket });
+
+    let relay = new osc.Relay(udp, socketPort, { raw: true });
+  });
+}

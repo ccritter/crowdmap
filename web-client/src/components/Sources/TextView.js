23b4d057d30bd9afe6c1b70cbe70db50b0dca862
Christopher Ritter <ritter.c@husky.neu.edu>
2019-11-19 12:55:14 -0500

    Bug fixes and changes to TextView

--- web-client/src/components/Sources/TextView.js
+++ web-client/src/components/Sources/TextView.js
@@ -3,22 +3,19 @@ import React from 'react';
 export default class RestView extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { value: '' };
-
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleKey = this.handleKey.bind(this)
   }
 
-  handleChange(event) {
-    this.props.config.socket.send({ address: this.props.config.destination })
-    this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
-    this.props.config.socket.send({ address: this.props.config.destination, args: [{ type: 's', value: this.state.value }] });
-    this.setState({ value: '' });
-    event.preventDefault();
+  handleKey(e) {
+    let value;
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      value = e.target.value;
+      e.target.value = '';
+    } else {
+      value = e.key;
+    }
+    if (value) this.props.config.socket.send({ address: this.props.config.destination, args: [{ type: 's', value }] });
   }
 
   render() {
@@ -26,9 +23,7 @@ export default class RestView extends React.Component {
     return (
       <div className="fullscreen">
         {this.props.config.prompt}
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="Type" onChange={this.handleChange} value={this.state.value}/>
-        </form>
+        <input type="text" placeholder="Type here" onKeyPress={this.handleKey}/>
       </div>
     )
   }
